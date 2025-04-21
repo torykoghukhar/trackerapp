ae579c7c7850f75108133108f769318d0c0b7cad
VIKA <vika.koghukhar@gmail.com>
2025-04-21 18:45:41 +0300

    changes

--- trackerapp/settings.py
+++ trackerapp/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -157,7 +156,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER="vika.koghukhar@gmail.com"
-EMAIL_HOST_PASSWORD="yxee lfpj ntjn inwj"
+EMAIL_HOST_USER = "vika.koghukhar@gmail.com"
+EMAIL_HOST_PASSWORD = "yxee lfpj ntjn inwj"
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
